50e5588485b3aadcf8e4e21941e2beba631f6d7c
singleshade <54930671+singleshade@users.noreply.github.com>
2019-09-05 17:26:12 +0530

    Update MOM.sql

--- sql/MOM.sql
+++ sql/MOM.sql
@@ -1,6 +1,6 @@
 
 create sequence emp_id 
 start with 100 increment by 1
-no cache no cycle;
+nocache nocycle;
 
 create table employees(emp_id int, emp_name varchar2(20), emp_email varchar2(20), emp_pass varchar2(20), emp_desg varchar2(20) , dept_id int , emp_status number(1) , emp_startdate date);
