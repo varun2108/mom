f289df30b3bf090cae682990851d6f34671cd733
AshutoshM <AshutoshM@DSK417-B1.quinnox.corp>
2019-09-05 13:16:44 +0530

    template for mvc arch.

---
+++ MoM/src/com/mom/model/Employees.java
@@ -0,0 +1,104 @@
+package com.mom.model;
+
+public class Employees {
+	private String emp_name,emp_mail,emp_password,emp_designation,emp_startdate;
+	private int emp_id,dept_id;
+	private boolean emp_status;
+	/**
+	 * @return the emp_name
+	 */
+	public String getEmp_name() {
+		return emp_name;
+	}
+	/**
+	 * @param emp_name the emp_name to set
+	 */
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	/**
+	 * @return the emp_mail
+	 */
+	public String getEmp_mail() {
+		return emp_mail;
+	}
+	/**
+	 * @param emp_mail the emp_mail to set
+	 */
+	public void setEmp_mail(String emp_mail) {
+		this.emp_mail = emp_mail;
+	}
+	/**
+	 * @return the emp_password
+	 */
+	public String getEmp_password() {
+		return emp_password;
+	}
+	/**
+	 * @param emp_password the emp_password to set
+	 */
+	public void setEmp_password(String emp_password) {
+		this.emp_password = emp_password;
+	}
+	/**
+	 * @return the emp_designation
+	 */
+	public String getEmp_designation() {
+		return emp_designation;
+	}
+	/**
+	 * @param emp_designation the emp_designation to set
+	 */
+	public void setEmp_designation(String emp_designation) {
+		this.emp_designation = emp_designation;
+	}
+	/**
+	 * @return the emp_startdate
+	 */
+	public String getEmp_startdate() {
+		return emp_startdate;
+	}
+	/**
+	 * @param emp_startdate the emp_startdate to set
+	 */
+	public void setEmp_startdate(String emp_startdate) {
+		this.emp_startdate = emp_startdate;
+	}
+	/**
+	 * @return the emp_id
+	 */
+	public int getEmp_id() {
+		return emp_id;
+	}
+	/**
+	 * @param emp_id the emp_id to set
+	 */
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+	/**
+	 * @return the dept_id
+	 */
+	public int getDept_id() {
+		return dept_id;
+	}
+	/**
+	 * @param dept_id the dept_id to set
+	 */
+	public void setDept_id(int dept_id) {
+		this.dept_id = dept_id;
+	}
+	/**
+	 * @return the emp_status
+	 */
+	public boolean isEmp_status() {
+		return emp_status;
+	}
+	/**
+	 * @param emp_status the emp_status to set
+	 */
+	public void setEmp_status(boolean emp_status) {
+		this.emp_status = emp_status;
+	}
+
+}
---
+++ MoM/src/com/mom/model/Login.java
@@ -0,0 +1,31 @@
+package com.mom.model;
+
+public class Login {
+	private String emp_password;
+	private int emp_id;
+	/**
+	 * @return the emp_password
+	 */
+	public String getEmp_password() {
+		return emp_password;
+	}
+	/**
+	 * @param emp_password the emp_password to set
+	 */
+	public void setEmp_password(String emp_password) {
+		this.emp_password = emp_password;
+	}
+	/**
+	 * @return the emp_id
+	 */
+	public int getEmp_id() {
+		return emp_id;
+	}
+	/**
+	 * @param emp_id the emp_id to set
+	 */
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+
+}
